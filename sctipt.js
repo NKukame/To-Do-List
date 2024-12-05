ae2f3b40be8202dfc932abe5f1660ee0be7c5c61
NKukame <NKukame@jhb.dvt.co.za>
2024-12-05 15:51:31 +0200

    Changed The isEmpty Code To Take Parameters

--- sctipt.js
+++ sctipt.js
@@ -1,7 +1,4 @@
-const activities = document.getElementById("activities");
-let dateTimeValue = document.getElementById('datetime');
-
-function isEmpty(){
+function isEmpty(activities, dateTimeValue){
     if(activities == '' && Date.parse(dateTimeValue) == null){
         alert("Please Enter All The Fields");
         return false;
@@ -10,9 +7,23 @@ function isEmpty(){
     return true;
 }
 
+// function showData(){
+//     var list;
+
+//     if(localStorage.getItem("list") == null){
+//         list = [];
+//     }else{
+//         list = JSON.parse(localStorage.getItem("list"));
+//     }
+
+// }
+
 function add(){
 
-    if(isEmpty() == true){
+    const activities = document.getElementById("activities");
+    const dateTimeValue = document.getElementById('datetime');
+
+    if(isEmpty(activities, dateTimeValue)){
         alert(activities.value);
         alert("Date Chosen: "+ dateTimeValue.value);
     }  
