09502f01808b6c319d83f3f4fdc65e85747599b0
NjabuloKukame <njabulokukame60@gmail.com>
2024-12-09 15:49:49 +0200

    Added Functionality

--- sctipt.js
+++ sctipt.js
@@ -1,5 +1,12 @@
-function isEmpty(activities, dateTimeValue){
-    if(activities == '' && Date.parse(dateTimeValue) == null){
+var activities = document.getElementById("activities").value;
+var dateTimeValue = document.getElementById("datetime").value;
+
+function isEmpty() {
+
+    activities = document.getElementById("activities").value;
+    dateTimeValue = document.getElementById("datetime").value;
+
+    if (activities === '' || dateTimeValue === '') {
         alert("Please Enter All The Fields");
         return false;
     }
@@ -7,26 +14,57 @@ function isEmpty(activities, dateTimeValue){
     return true;
 }
 
-// function showData(){
-//     var list;
+function displayData() {
+    var list;
 
-//     if(localStorage.getItem("list") == null){
-//         list = [];
-//     }else{
-//         list = JSON.parse(localStorage.getItem("list"));
-//     }
+    if (localStorage.getItem("list") == null) {
+        list = [];
+    } else {
+        list = JSON.parse(localStorage.getItem("list"));
+    }
 
-// }
+    var html = "";
 
-function add(){
+    list.forEach(function (element, index) {
+        html += "<tr>";
+        html += "<td>" + element.activities + "</td>";
+        html += "<td>" + element.dateTimeValue + "</td>";
+        html += '<td><button onclick="deleteData(' +
+            index +
+            ')" class= "delete">Delete</button><button onclick="deleteData(' + index + ')" class= "edit">Edit</button></td>';
+        html += "</tr>";
+    });
 
-    const activities = document.getElementById("activities");
-    const dateTimeValue = document.getElementById('datetime');
+    document.querySelector("#activitiesTable tbody").innerHTML = html;
 
-    if(isEmpty(activities, dateTimeValue)){
-        alert(activities.value);
-        alert("Date Chosen: "+ dateTimeValue.value);
-    }  
-    
-    
+}
+
+document.onload = displayData();
+
+function add() {
+
+    if (isEmpty()) {
+        activities = document.getElementById("activities").value;
+        dateTimeValue = document.getElementById("datetime").value;
+
+        var list;
+
+        if (localStorage.getItem("list") == null) {
+            list = [];
+        } else {
+            list = JSON.parse(localStorage.getItem("list"));
+        }
+
+        list.push({
+            activities : activities,
+            dateTimeValue : dateTimeValue,
+        });
+
+        localStorage.setItem("list", JSON.stringify
+        (list));
+        displayData();
+        document.getElementById("activities").value = "";
+        document.getElementById("datetime").value = "";
+    }
+
 }
